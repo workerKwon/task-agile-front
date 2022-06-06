4da6cdcbf92f6b1e18895244ac7664166a9d5709
workerKwon <dmdmdm1322@gmail.com>
2022-06-06 13:36:58 +0900

    Add i18n

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -1,5 +1,35 @@
+import { useTranslation } from 'react-i18next'
+
 function PageFooter() {
-  return <></>
+  const { t } = useTranslation()
+
+  return (
+    <>
+      <footer className='footer'>
+        <span className='copyright'>&copy; 2018 TaskAgile.com</span>
+        <ul className='footer-links list-inline float-right'>
+          <li className='list-inline-item'>
+            <a href='#'>{t('pageFooter.about')}</a>
+          </li>
+          <li className='list-inline-item'>
+            <a href='#'>{t('pageFooter.termOfService')}</a>
+          </li>
+          <li className='list-inline-item'>
+            <a href='#'>Privacy Policy</a>
+          </li>
+          <li className='list-inline-item'>
+            <a
+              href='https://github.com/taskagile/vuejs.spring-boot.mysql'
+              target='_blank'
+              rel='noreferrer'
+            >
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </>
+  )
 }
 
 export default PageFooter
