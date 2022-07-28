5a8411931042b61811bf341942f91f0b05f9caec
workerKwon <dmdmdm1322@gmail.com>
2022-07-28 17:21:53 +0900

    Update add ^local-file proxy on dev

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
       '^/api/*': {
         target: 'http://localhost:8080',
         changeOrigin: true
+      },
+      '^/local-file/': {
+        target: 'http://localhost:8080'
       }
     }
   }
